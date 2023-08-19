826a5921eb7d09e6eccbad375009c7f4763e0ade
seunggyun-jeong <jskyun98@gmail.com>
2023-08-19 15:00:43 +0900

    create project

---
+++ Junction2023-FinlandSauna/Junction2023-FinlandSauna/Junction2023_FinlandSaunaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Junction2023_FinlandSaunaApp.swift
+//  Junction2023-FinlandSauna
+//
+//  Created by 정승균 on 2023/08/19.
+//
+
+import SwiftUI
+
+@main
+struct Junction2023_FinlandSaunaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
